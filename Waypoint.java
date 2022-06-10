1a71368165bbc3efe6f5c43e79aa49c26a62f9ae
robbhillhouse <54416463+robbhillhouse@users.noreply.github.com>
2022-06-09 23:18:19 -0400

    Add files via upload

--- Waypoint.java
+++ Waypoint.java
@@ -1,9 +1,23 @@
-public class Waypoint implements Comparable<Waypoint> {
+/**
+ * @version April 27 2022
+ * @author 23hillhouse
+ * turns all parameters used to sort into objects to be used in other code
+ */
+public class Waypoint {
     private String type, name, state;
     private double toSpringer, toKatahdin;
     private int elevation;
 
-    public Waypoint(String t, String n, String s, double ts, double tk, int e) {
+    /**
+     * parameterized constructor of the Waypoint class
+     * @param t type
+     * @param n name
+     * @param s state
+     * @param ts distance to springer
+     * @param tk distance to Katahdin
+     * @param e elevation
+     */
+    public Waypoint(String t, String n, String s, double ts, double tk, int e){
         type = t;
         name = n;
         state = s;
@@ -12,34 +26,69 @@ public class Waypoint implements Comparable<Waypoint> {
         elevation = e;
     }
 
-    public Waypoint(Waypoint temp) {
+    /**
+     * constructor for Waypoint
+     */
+    public Waypoint(){
+        type = "";
+        name = "";
+        state = "";
+        toSpringer = 0.0;
+        toKatahdin = 0.0;
+        elevation = 0;
     }
 
+    /**
+     * gets "type" when the method is called
+     * @return type
+     */
     public String getType() {
         return type;
     }
-
+    /**
+     * gets "name" when the method is called
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * gets "state" when the method is called
+     * @return state
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * gets "toSpringer" when the method is called
+     * @return toSpringer
+     */
     public double getToSpringer() {
         return toSpringer;
     }
 
+    /**
+     * gets "toKatahdin" when the method is called
+     * @return toKatahdin
+     */
     public double getToKatahdin() {
         return toKatahdin;
     }
 
+    /**
+     * gets "elevation" when the method is called
+     * @return elevation
+     */
     public int getElevation() {
         return elevation;
     }
 
-    @Override
+    /**
+     * turns object into a usable String format
+     * @return string of object that was called
+     */
     public String toString() {
         return "Waypoint{" +
                 "type='" + type + '\'' +
@@ -50,9 +99,5 @@ public class Waypoint implements Comparable<Waypoint> {
                 ", elevation=" + elevation +
                 '}';
     }
-    public int compareTo(Waypoint other)  {
-        Double d1 = this.toKatahdin;
-        Double d2 = other.toKatahdin;
-        return d1.compareTo(d2);
-    }
+
 }
--- WaypointComparator.java
+++ WaypointComparator.java
@@ -1,45 +1,72 @@
 import java.util.Comparator;
 
+/**
+ * @version April 27 2022
+ * @author 23hillhouse
+ * Comparator class for waypoints
+ */
 public class WaypointComparator implements Comparator<Waypoint> {
     private int category;
     private boolean asc;
 
-    public WaypointComparator(int c, boolean a) {
+    /**
+     * parameter constructor for a waypoint comparator
+     * @param c - category used for comparing
+     * @param a - a boolean counting the ascending or descending values
+     */
+    public WaypointComparator(int c, boolean a){
         category = c;
         asc = a;
     }
 
-    public WaypointComparator(int c)    {
+    /**
+     * parameter constructor for a waypoint comparator
+     * @param c - category used for comparing
+     */
+    public WaypointComparator(int c){
         category = c;
-        asc = true;
+        asc= true;
     }
 
-    public WaypointComparator() {
+    /**
+     * constructor for waypoint comparator
+     */
+    public WaypointComparator(){
         category = 4;
         asc = true;
     }
 
-    public int compare(Waypoint one, Waypoint two)  {
+    /**
+     * returns waypoint one minus waypoint two
+     * @param one - waypoint being compared 1
+     * @param two - waypoint being compared 2
+     * @return - outcome of waypoint one minus waypoint two
+     */
+    public int compare(Waypoint one, Waypoint two){
         int diff = 0;
-        if(category == 1)  // TYPE
+        if(category ==1){
             diff = one.getType().compareTo(two.getType());
-        else if(category == 2)  // NAME
-            diff = one.getName().compareTo(two.getName());
-        else if(category == 3)  // STATE
-            diff = one.getState().compareTo(two.getState());
-        else if(category == 4) { // DTK
-            Double d1 = one.getToKatahdin();
-            Double d2 = two.getToKatahdin();
-            diff = d1.compareTo(d2);
         }
-        else if(category == 5) {  // DTS
-            Double d1 = one.getToSpringer();
+        else if(category == 2){
+            diff = one.getName().compareTo(two.getName());
+        }
+        else if(category == 3){
+            diff = one.getState().compareTo(two.getState());
+        }
+        else if(category == 4){
+            Double d1= one.getToSpringer();
             Double d2 = two.getToSpringer();
             diff = d1.compareTo(d2);
         }
-        else if(category == 6) // DTS
-            diff = one.getElevation() - two.getElevation();
+        else if(category == 5){
+            Double d1= one.getToKatahdin();
+            Double d2 = two.getToKatahdin();
+            diff = d1.compareTo(d2);
+        }
+        else if(category == 6){
+            diff = one.getElevation()- two.getElevation();
+        }
 
-        return (asc) ? diff : -diff;
+        return(asc)? diff: -diff;
     }
 }
